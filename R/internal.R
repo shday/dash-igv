e418420b8e0485f5f37800d35398681fa82c23c5
Stephen Day <stephen.h.day@gmail.com>
2020-05-29 17:09:30 -0400

    first build

--- R/internal.R
+++ R/internal.R
@@ -10,6 +10,6 @@ version = "0.0.1", src = list(href = NULL,
 file = "deps"), meta = NULL,
 script = 'dash_igv.min.js.map',
 stylesheet = NULL, head = NULL, attachment = NULL, package = "dashIgv",
-all_files = FALSE), class = "html_dependency"))
+all_files = FALSE, dynamic = TRUE), class = "html_dependency"))
 return(deps_metadata)
 }
