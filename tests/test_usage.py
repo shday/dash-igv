2df471ed7989e2f67b77cc62c30ac37937398136
Stephen Day <stephen.h.day@gmail.com>
2020-06-15 23:03:35 -0400

    added basic test

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -2,24 +2,18 @@ from dash.testing.application_runners import import_app
 
 
 # Basic test for the component rendering.
-# The dash_duo pytest fixture is installed with dash (v1.0+)
 def test_render_component(dash_duo):
     # Start a dash app contained as the variable `app` in `usage.py`
     app = import_app('usage')
     dash_duo.start_server(app)
 
-    # Get the generated component input with selenium
-    # The html input will be a children of the #input dash component
-    my_component = dash_duo.find_element('#input > input')
+    # Check that the genome loaded
+    dash_duo.wait_for_text_to_equal('#igv-current_genome', 'ASM985889v3')
 
-    assert 'my-value' == my_component.get_attribute('value')
+    # Check that both tracks leaded
+    tracks = dash_duo.find_elements('.igv-track-label')
 
-    # Clear the input
-    dash_duo.clear_input(my_component)
+    assert 'Annotations' == tracks[0].text
+    assert 'Genes' == tracks[1].text
 
-    # Send keys to the custom input.
-    my_component.send_keys('Hello dash')
 
-    # Wait for the text to equal, if after the timeout (default 10 seconds)
-    # the text is not equal it will fail the test.
-    dash_duo.wait_for_text_to_equal('#output', 'You have entered Hello dash')
